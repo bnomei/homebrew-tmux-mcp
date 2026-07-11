bb98671803e77fcca6a1c954567a01ce792c5cec
Bruno Meilick <b@bnomei.com>
2026-07-11 23:11:26 +0100

    :bookmark: v0.6.0

--- Formula/tmux-mcp-rs.rb
+++ Formula/tmux-mcp-rs.rb
@@ -1,14 +1,14 @@
 class TmuxMcpRs < Formula
   desc "Tmux MCP server in Rust"
   homepage "https://github.com/bnomei/tmux-mcp"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   checksums = {
-    aarch64_apple_darwin: "1e70a11f4ddec30b1a586b0c5b40f24524515ebdfa81d304f6cf4638981b8599",
-    x86_64_apple_darwin: "0ae4275fab23ce1a5cca805239c99271e67c864005b77b9f624dc772a12318de",
-    aarch64_unknown_linux_musl: "7aecc33bbb7c4a37c0a3896d0a248a64a27e59fc142ecd3393a2ec9397c9a3dd",
-    x86_64_unknown_linux_musl: "f1f88bafbbcb4fffce84539dd7cc8c4d2a6adc5a46d9892fe9dd3961db746b0f",
+    aarch64_apple_darwin: "5764c2f1bcf7c4eb3afc7ed23ccb47bbf7283fcf2cda6add801dddb10313a0ed",
+    x86_64_apple_darwin: "50313638aac4015258e8c871d70748a2604861aa21f4cca6d5e7ec46c273aedb",
+    aarch64_unknown_linux_musl: "7afd734bbec5dc825031c94181ae944bd5511c574967bae5e8f0a694db0607d9",
+    x86_64_unknown_linux_musl: "6d2042b74bb1f35ef6e274a15d72a99a621fcd178c26367ed076dfd92dba786d",
   }
 
   on_macos do
